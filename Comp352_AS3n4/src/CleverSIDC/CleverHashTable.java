4098c29fc0521ba712abe95110caf16944e2b65d
Alex Flores <SorAxel360@hotmail.com>
2021-12-05 19:34:36 -0500

    Reset the files

--- Comp352_AS3n4/src/CleverSIDC/CleverHashTable.java
+++ Comp352_AS3n4/src/CleverSIDC/CleverHashTable.java
@@ -1,4 +1,7 @@
+
 package CleverSIDC;
+
+import java.util.ArrayList; // Will not be used to construct the data structure itself.
 
 class CleverHashTable 
 {
@@ -14,11 +17,17 @@ class CleverHashTable
 	Key first; // The first/leftmost key in the table.
 	Key last; // The last/rightmost key in the table.
 	
+	
+	
+	
+	
 	public CleverHashTable(int tableSize)
 	{
 		setSIDCThreshold(tableSize);
 		//System.out.println(String.format("Table initialized with length: %d.\n\n", table.length));
 	}
+	
+	
 	
 	void setSIDCThreshold(int size)
 	{
@@ -58,43 +67,67 @@ class CleverHashTable
 	}
 	
 	
-	void printLoad()
+	void printInfo()
 	{
-		System.out.println(String.format("The hash table currently contains %d items.", load));
+		System.out.println(String.format("The hash table has %d buckets in total and currently contains %d items.", ceiledSize, load));
+		System.out.println(String.format("Its first key is #%s at index %d.", first.toString(), first.index));
+		System.out.println(String.format("Its last key is #%s at index %d.\n", last.toString(), last.index));
 	}
 	
 	void add(int SIDC, Student value)
 	{
 		int hash = hash(SIDC);
+		Key newKey = null;
 		
 		if (table[hash] == null)
 		{
-			table[hash] = new Key(SIDC, value, hash);
+			table[hash] = newKey = new Key(SIDC, value, hash);
+			this.setUpPrevNext(newKey);
 		}
-		else if (table[hash] != null) // We have a collision.
+		else if (table[hash].key < 0) // We have encountered a previously deleted key.
+		{
+			newKey = new Key(SIDC, value, hash);
+			newKey.setPrev(table[hash].prevKey); // These functions handle both regular neighbors and chained keys.
+			newKey.setNext(table[hash].nextKey);
+			table[hash] = newKey;
+		}
+		else // We have a collision.
 		{
 			if (ceiledSize <= 1000) // If the table only has a capacity of a thousand or less, 
 			{
-				while(table[hash] != null) // resolve the collision through linear probing.
+				while((hash++ < ceiledSize) && (table[hash] != null)) // resolve the collision through linear probing.
 				{
 					hash++;
 				}
-				table[hash] = new Key(SIDC, value, hash);
-			}
-			else // If the array is larger, resolve the collision through separate chaining.
-			{
-				Key parentKey = table[hash];
-				//System.out.println(String.format("Parent key #%s at index %d.", parentKey.toString(), hash));
-				
-				while ((parentKey.nextKey != null) && (parentKey.nextKey.isChained = true)) // Find the last chained element.
+				if (hash < ceiledSize) // If the current hash exceeds the table's capacity, continue on to the separate chaining section.
 				{
-					parentKey = parentKey.nextKey;
-					//System.out.println(String.format("Moving on to key #%d.\n", parentKey.key));
+					table[hash] = newKey = new Key(SIDC, value, hash);
+					this.setUpPrevNext(newKey);
+					load++;
+					return;
 				}
-				parentKey.setNext(new Key(SIDC, value, hash, true)); // Create a chained key.
+			}
+			
+			// If the array is larger, resolve the collision through separate chaining.
+			Key parentKey = table[hash];
+			while ((parentKey.nextKey != null) && (parentKey.index == parentKey.nextKey.index)) // Find the last chained element.
+			{
+				parentKey = parentKey.nextKey;
+			}
+			
+			newKey = new Key(SIDC, value, hash, true); // Create a chained key.
+			if (parentKey.equals(this.last)) // If the parent key was the last element in the array, update it.
+			{
+				this.last = newKey;
+			}
+			if (parentKey.nextKey != null) // If the parent key has a following key, update the relevant parameters.
+			{
+				newKey.setNext(parentKey.nextKey);
+				parentKey.setNext(newKey);
+
+				newKey.printPrevNext();
 			}
 		}
-		
 		load++;
 		return;
 	}
@@ -145,47 +178,249 @@ class CleverHashTable
 		return hash;
 	}
 	
+	void setUpPrevNext(Key key) // Allows for negative/deleted keys to keep existing in the chain.
+	{
+		// Traverse the array in search of a previous item.
+		for (int currentIndex = key.index - 1; currentIndex >= 0; currentIndex--)
+		{
+			if (table[currentIndex] != null) // When a first item is found, set and move on.
+			{
+				Key currentKey = table[currentIndex];
+				while ((currentKey.nextKey != null) && (currentKey.nextKey.isChained = true) && (currentKey.index == currentKey.nextKey.index)) // If needed, handle chained keys.
+				{
+					currentKey = currentKey.nextKey; 
+				}
+				key.setPrev(currentKey);
+				break;
+			}
+		}
+		if (key.prevKey == null) // If no item was found, the key�s prev variable will have remained null.
+		{
+			this.first = key; // This means the key is the first item in the array.
+		}
+		
+		// Traverse the array in search of a subsequent item.
+		for (int currentIndex = key.index + 1; currentIndex < ceiledSize; currentIndex++)
+		{
+			if (table[currentIndex] != null) // When a first item is found, set and move on.
+			{
+				key.setNext(table[currentIndex]);
+				break;
+			}
+		}
+		if (key.nextKey == null) // If no item was found, the key�s next variable will have remained null.
+		{
+			this.last = key; // This means the key is the last item in the array.
+		}
+		
+		System.out.println(String.format("Student #%s (index %d) is preceded by student #%s (index %d), and followed by student #%s (index %d).\n", 
+				key.toString(), key.index, 
+				key.prevKey, (key.prevKey != null ? key.prevKey.index : -1),
+				key.nextKey, (key.nextKey != null ? key.nextKey.index : -1)));
+	}
+	
+	void setUpNext(Key key)  // Allows for negative/deleted keys to keep existing in the chain.
+	{
+		// Traverse the array in search of a subsequent item.
+		for (int currentIndex = key.index + 1; currentIndex < ceiledSize; currentIndex++)
+		{
+			if (table[currentIndex] != null) // When a first item is found, set and move on.
+			{
+				key.setNext(table[currentIndex]);
+				break;
+			}
+		}
+		if (key.nextKey == null) // If no item was found, the key�s next variable will have remained null.
+		{
+			this.last = key; // This means the key is the last item in the array.
+		}
+		
+		System.out.println(String.format("Student #%s (index %d) is preceded by student #%s (index %d), and followed by student #%s (index %d).\n", 
+				key.toString(), key.index, 
+				key.prevKey, (key.prevKey != null ? key.prevKey.index : -1),
+				key.nextKey, (key.nextKey != null ? key.nextKey.index : -1)));
+	}
+	
 	
 	// Return the specific item associated with the inputed SIDC key.
-	Student getValue(int key)
+	Student getValue(int SIDC)
 	{
-		int hash = hash(key);
+		int hash = hash(SIDC);
 		
-		if (table[hash] == null)
+		if ((table[hash] == null) || table[hash].key == -SIDC) // If the key corresponds to the negative value of the inputed SIDC, it has been deleted.
 		{
-			System.out.println(String.format("No entry found for SIDC key '%d', expected to be found at index %d.\n", key, hash));
+			//System.out.println(String.format("No entry found for SIDC key '%d', expected to be found at index %d.\n", SIDC, hash));
 			return null;
 		}
 		else
 		{
 			Key targetKey = table[hash]; // Find the first item at index.
-
-			while(!targetKey.equals(key)) // If it's not the right key, move on to the next item.
+			while(!targetKey.equals(SIDC) && (targetKey.nextKey != null)) // If it's not the right key, move on to the next item.
 			{
-				if (targetKey.nextKey != null)
+				if (targetKey.equals(SIDC))
 				{
-					targetKey = targetKey.nextKey;
-				}
-				else
+					System.out.println("Found target key!.");
+					break;
+				} // If the deleted key is encountered, there is no need to continue on.
+				else if (targetKey.equals(-SIDC) || (!targetKey.equals(this.last))) 
 				{
-					System.out.println(String.format("No entry found for SIDC key '%d', expected to be found at index %d.\n", key, hash));
+					System.out.println(String.format("No entry found for SIDC key '%d', expected to be found at index %d.\n", SIDC, hash));
 					return null;
 				}
+				targetKey = targetKey.nextKey;
 			}
 			
-			System.out.println(String.format("Found taget key #%d, returning associated Student.\n", key));
+			System.out.println(String.format("Found taget key #%d. Returning associated Student.\n", SIDC));
+			//targetKey.printPrevNext();
 			return targetKey.value;
 		}
-
-		return new Student();
 	}
 	
-	//
-	/*
-	Student getValues(int key)
+	// Return all values chained to the hash generated from the SIDC key.
+	ArrayList<Student> getValues(int SIDC) // ADD HANDLING OF NEGATIVE/DELETED KEYS
 	{
-		return new Student();
+		int hash = hash(SIDC);
+		
+		if (table[hash] == null)
+		{
+			System.out.println(String.format("No entry found for SIDC key '%d', expected to be found at index %d.\n", SIDC, hash));
+			return null;
+		}
+		
+		ArrayList<Student> studentsFound = new ArrayList<Student>();
+		
+		if (ceiledSize <= 1000) // If the table's small size means linear probing was used instead of chaining,
+		{
+			studentsFound.add(this.getValue(SIDC)); // seek and return only the specific student.
+			return studentsFound;
+		}
+		else
+		{
+			Key targetKey = table[hash]; // Find the first item at index.
+			studentsFound.add(targetKey.value); // Beware, it will not be marked as 'chained'.
+			
+			while ((!targetKey.equals(this.last)) && (targetKey.nextKey != null) && (targetKey.nextKey.isChained = true) && (targetKey.nextKey.index == hash)) // Collect chained elements.
+			{
+				if (targetKey.key >= 0) // Only collect 'living'/nonegative keys.
+				{
+					studentsFound.add(targetKey.value);
+				}
+				targetKey = targetKey.nextKey;
+			}
+		}
+		System.out.println(String.format("%d chained key(s) were/was found at index %d. Returning associated Student(s).\n", studentsFound.size(), hash));
+		return studentsFound;
 	}
-	*/
+	
+	// 'Removes' the key corresponding to the given SIDC code by negating it. It will no longer be accessible to getValue(s) operations, but might remain part of the prev/next chain.
+	Key remove(int SIDC)
+	{
+		int hash = hash(SIDC);
+		Key returnKey;
+		
+		if (table[hash] == null) // Nothing was found at the hash index generated from the SIDC key.
+		{
+			System.out.println(String.format("No key was found for SIDC code #%s. It may already have been deleted.", SIDC));
+			return null;
+		}
+		
+		// Traverse the array in search of the relevant key.
+		returnKey = table[hash];
+		while (!returnKey.equals(SIDC))
+		{
+			if ((returnKey.equals(-SIDC)) || (returnKey.equals(this.last)) || (returnKey.nextKey == null)) // If the key has already been deleted, or was never found.
+			{
+				System.out.println(String.format("No key was found for SIDC code #%s. It may already have been deleted.", SIDC));
+				return null;
+			}
+			returnKey = returnKey.nextKey;
+		}
+		
+		// If the key to be deleted is the root of a chain, replaced it with the next in line.
+		if ((!returnKey.isChained) && (returnKey.nextKey.isChained) && (returnKey.index == returnKey.nextKey.index))
+		{
+			table[returnKey.index] = returnKey.nextKey;
+			if (returnKey.prevKey != null)
+			{
+				table[returnKey.index].setPrev(returnKey.prevKey);
+			}
+			table[returnKey.index].isChained = false;
+			if (returnKey.equals(this.first)) // If the key was the first in the chain, update the hashTable�s first key parameter.
+			{
+				this.first = table[returnKey.index];
+			}
+		}
+		else // Else, simply �negate� the key to mark it as deleted.
+		{
+			returnKey.key = -1 * returnKey.key;
+			if (returnKey.equals(this.first)) // If the key was the first in the chain, update the hashTable�s first key parameter.
+			{
+				this.first = returnKey.nextKey;
+			}
+			if (returnKey.equals(this.last)) // If the key was the last in the chain, update the hashTable�s last key parameter.
+			{
+				this.last = returnKey.prevKey;
+			}
+		}
+		
+		System.out.println(String.format("Key #%d has been removed from index %d.\n", Math.abs(returnKey.key), returnKey.index));
+		this.load --;
+		return returnKey;
+	}
+	
+	/*List<key> allKeys()
+	{
+		List<Keys> keyList = new ArrayList();
+		Key key = first;
+
+		while (key.nextKey != last)
+		{
+
+		}
+	}*/
+
+	int rangeKey(int key1, int key2)
+	{
+		if (load < 0)
+		{
+			System.out.println(String.format("The table has no entries.\n"));
+			return 0;
+		} 
+		else if (key1 == key2) 
+		{
+			System.out.println(String.format("Both keys are the same."));
+			return 0;
+		}
+
+		int range = 0;
+		int hash1 = hash(key1);
+		int hash2 = hash(key2);
+		
+		if (table[hash1] == null || table[hash2] == null)
+		{
+			System.out.println(String.format("No entry found for one or both of the SIDC keys '%d' and '%d'.\n", key1, key2));
+			return 0;
+		}
+
+		Key k1 = table[hash1];
+		Key k2 = table[hash2];
+
+		Key kInBetween = k1;
+		while (kInBetween.nextKey != k2)
+		{
+			if (kInBetween.key > 0)
+			{
+				range++;
+			}
+			System.out.println("key : " + kInBetween);
+			System.out.println("key : " + kInBetween.nextKey);
+
+
+			kInBetween = kInBetween.nextKey;
+
+		}
+
+		return range;
+	}
 
 }
--- Comp352_AS3n4/src/CleverSIDC/Main.java
+++ Comp352_AS3n4/src/CleverSIDC/Main.java
@@ -1,6 +1,5 @@
 package CleverSIDC;
 
-import java.util.Hashtable;
 import java.util.Random;
 
 public class Main
@@ -9,27 +8,70 @@ public class Main
     {
         System.out.println("Hello World!");
         
+        CleverHashTable testHashTable0 = new CleverHashTable(643);
         CleverHashTable testHashTable1 = new CleverHashTable(2445);
         CleverHashTable testHashTable2 = new CleverHashTable(76642);
         CleverHashTable testHashTable3 = new CleverHashTable(526153);
-        
-        CleverHashTable testHashTable4 = new CleverHashTable(9999);
-        CleverHashTable testHashTable5 = new CleverHashTable(99999);
-        
-        CleverHashTable testHashTable6 = new CleverHashTable(785);
-        testHashTable6.add(55555555, new Student());
-        testHashTable6.add(55555556, new Student());
-        
-        testHashTable3.add(83502412, new Student());
-        testHashTable3.add(83502413, new Student());
-        testHashTable3.add(83502431, new Student());
-        testHashTable3.add(83502341, new Student());
-        testHashTable3.add(83502342, new Student());
 
-        System.out.println(generate(testHashTable3));
-        System.out.println(generate(testHashTable3));
-        System.out.println(generate(testHashTable3));
-        System.out.println(generate(testHashTable3));
+        testHashTable3.add(87654321, new Student());
+        testHashTable3.add(87654323, new Student());
+        testHashTable3.add(87654322, new Student());
+        
+        testHashTable3.printInfo();
+        
+        testHashTable3.add(24352340, new Student());
+        testHashTable3.add(24352430, new Student());
+        testHashTable3.add(24352034, new Student());
+        
+        testHashTable3.add(24352341, new Student());
+        
+        
+        testHashTable3.printInfo();
+        
+        testHashTable3.getValue(87654322);
+        testHashTable3.getValue(24352430);
+        
+        testHashTable3.getValues(24352430);
+        
+        
+        testHashTable3.remove(87654323);
+        testHashTable3.getValue(87654323);
+        testHashTable3.printInfo();
+        
+        
+        testHashTable3.remove(24352340);
+        testHashTable3.getValue(24352340);
+        testHashTable3.getValue(24352430);
+        testHashTable3.getValues(24352340);
+        
+        
+        testHashTable3.printInfo();
+        
+        System.out.println("\nGenerated " + generate(testHashTable3));
+        System.out.println("Generated " + generate(testHashTable3));
+        System.out.println("Generated " + generate(testHashTable3));
+        System.out.println("Generated " + generate(testHashTable3) + "\n");
+
+        CleverHashTable testHashTable = new CleverHashTable(2445);
+
+        testHashTable.add(44352340, new Student());
+        testHashTable.add(44352378, new Student());
+        testHashTable.add(44352363, new Student());
+        //testHashTable.add(44352347, new Student());
+        //testHashTable.add(44352657, new Student());
+
+        /*testHashTable0.add(generate(testHashTable0), new Student());
+        testHashTable0.add(generate(testHashTable0), new Student());
+        testHashTable0.add(generate(testHashTable0), new Student());
+        testHashTable0.add(generate(testHashTable0), new Student());
+        testHashTable0.add(generate(testHashTable0), new Student());
+        testHashTable0.add(generate(testHashTable0), new Student());
+        testHashTable0.add(generate(testHashTable0), new Student());*/
+        testHashTable.add(44352341, new Student());
+
+        System.out.println("Range values with same values: " + testHashTable.rangeKey(44352340, 44352340) + "\n");
+        System.out.println("Range values with wrong values: " + testHashTable.rangeKey(12345678, 23459456) + "\n");
+        System.out.println("\nRange values with correct values: " + testHashTable.rangeKey(44352340, 44352341) + "\n");
 
     }
 
